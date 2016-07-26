da5652a17342ac8bcc8a93822549dbf46dff6f04
rokoDev <mishaDoter@gmail.com>
2016-07-26 16:04:15 +0300

    first commit

---
+++ MyTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MyTest
+//
+//  Created by roko on 7/26/16.
+//  Copyright © 2016 roko. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
